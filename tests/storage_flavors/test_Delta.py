dee2248a2fb4ddfc1d97b5acb6797e4eeaee49c7
Gabriel Pehls <gabrielpehls@hotmail.com>
2024-02-25 16:45:35 -0300

    creating unit tests

---
+++ tests/storage_flavors/test_Delta.py
@@ -0,0 +1,7 @@
+import pytest
+import os
+from mlops_structure.storage_flavors.Delta import delta_storage
+
+def test_basepath_is_created_at_init():
+    lib_storage = delta_storage(store_name='test_storage')
+    assert os.path.exists(lib_storage.base_path)
