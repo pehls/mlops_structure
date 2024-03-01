b3909fd9d798e0617d801ce045adafab24948478
Gabriel Pehls <gabrielpehls@hotmail.com>
2024-03-01 02:55:17 -0300

    aaaaa

--- mlops_structure/__init__.py
+++ mlops_structure/__init__.py
@@ -4,6 +4,6 @@ __author__ = """Gabriel Pehls @ Tropical Brain"""
 __email__ = "gabrielpehls@hotmail.com"
 __version__ = "0.1.0"
 
-from structure import feature_store
-from contracts.storage import interface_storage
-from storage_flavors.delta import delta_storage
+from mlops_structure.structure import feature_store
+from mlops_structure.contracts.storage import interface_storage
+from mlops_structure.storage_flavors.delta import delta_storage
