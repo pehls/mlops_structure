4d7c89bd97ded2390477200f0b39d8cc0caffe9f
Gabriel Pehls <gabrielpehls@hotmail.com>
2024-03-01 02:59:38 -0300

    adjust import in structure

--- mlops_structure/structure.py
+++ mlops_structure/structure.py
@@ -1,5 +1,5 @@
-from .contracts.storage import interface_storage
-from .storage_flavors.delta import delta_storage
+from mlops_structure.contracts.storage import interface_storage
+from mlops_structure.storage_flavors.delta import delta_storage
 class feature_store(object):
     """
     Example
