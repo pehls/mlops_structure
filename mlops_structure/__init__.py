d75721f56272db03feb45ab946e3b48ee6140bae
Gabriel Pehls <gabrielpehls@hotmail.com>
2024-03-01 02:40:38 -0300

    alter package init

--- mlops_structure/__init__.py
+++ mlops_structure/__init__.py
@@ -4,8 +4,6 @@ __author__ = """Gabriel Pehls @ Tropical Brain"""
 __email__ = "gabrielpehls@hotmail.com"
 __version__ = "0.1.0"
 
-from mlops_structure.storage_flavors import delta
-from mlops_structure.storage_flavors.delta import *
-
-from mlops_structure.contracts import storage
-from mlops_structure.contracts.storage import *
+from mlops_structure.structure import feature_store
+from mlops_structure.contracts.storage import interface_storage
+from mlops_structure.storage_flavors.delta import delta_storage
